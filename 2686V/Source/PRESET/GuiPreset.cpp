8be6423e43aa4e709eea333f531fd01f4b6dfa9c
Cyross Makoto <takuan45189@gmail.com>
2026-03-07 00:08:35 +0900

    プリセットをリセットした際、OPZX3のファイル名ラベルが初期化されていなかったバグを修正

--- 2686V/Source/PRESET/GuiPreset.cpp
+++ 2686V/Source/PRESET/GuiPreset.cpp
@@ -199,6 +199,7 @@ void GuiPreset::setup()
                     // ファイル名表示のクリア
                     ctx.editor.updateRhythmFileNames(Io::empty);
                     ctx.editor.updateAdpcmFileName(Io::empty);
+                    ctx.editor.updateOpzx3FileNames(Io::empty);
                 }
             }
         );
