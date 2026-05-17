5d89e25150b5ed2832ac1d3cc222fa1b00943779
Cyross Makoto <takuan45189@gmail.com>
2026-05-17 18:07:15 +0900

    FXのUI調整

--- 2686V/Source/Gui/Fx/Core.h
+++ 2686V/Source/Gui/Fx/Core.h
@@ -24,51 +24,39 @@ class GuiFx : public GuiBase
 
     // トレモロ(Tremolo)
     GuiToggleButton tBypassBtn;
-    GuiCategoryLabel tMainCat;
     GuiSlider tRateSlider, tDepthSlider;
-    GuiCategoryLabel tMixCat;
     GuiSlider tMixSlider;
     GuiTextButton tDryBtn, tHalfBtn, tWetBtn;
 
     // ビブラート(Vibrato)
     GuiToggleButton vBypassBtn;
-    GuiCategoryLabel vMainCat;
     GuiSlider vRateSlider, vDepthSlider;
-    GuiCategoryLabel vMixCat;
     GuiSlider vMixSlider;
     GuiTextButton vDryBtn, vHalfBtn, vWetBtn;
 
     // ビットクラッシャー(Modern Bit Crusher)
     GuiToggleButton mbcBypassBtn;
-    GuiCategoryLabel mbcMainCat;
     GuiSlider mbcRateSlider, mbcBitsSlider;
-    GuiCategoryLabel mbcMixCat;
     GuiSlider mbcMixSlider;
     GuiTextButton mbcDryBtn, mbcHalfBtn, mbcWetBtn;
 
     // ディレイ(Delay)
     GuiToggleButton dBypassBtn;
-    GuiCategoryLabel dMainCat;
     GuiSlider dTimeSlider, dFbSlider;
-    GuiCategoryLabel dMixCat;
     GuiSlider dMixSlider;
     GuiTextButton dDryBtn, dHalfBtn, dWetBtn;
 
     // Reverb
     GuiToggleButton rBypassBtn;
-    GuiCategoryLabel rMainCat;
     GuiSlider rSizeSlider, rDampSlider;
-    GuiCategoryLabel rMixCat;
     GuiSlider rMixSlider;
     GuiTextButton rDryBtn, rHalfBtn, rWetBtn;
 
     // Filter
     GuiToggleButton flBypassBtn;
-    GuiCategoryLabel flMainCat;
     GuiComboBox flTypeSelector;
     GuiSlider flFreqSlider;
     GuiSlider flQSlider;
-    GuiCategoryLabel flMixCat;
     GuiSlider flMixSlider;
     GuiTextButton flDryBtn, flHalfBtn, flWetBtn;
 public:
@@ -84,56 +72,44 @@ public:
         bypassToggle(context),
         resetBtn(context),
         tBypassBtn(context),
-        tMainCat(context),
         tRateSlider(context),
         tDepthSlider(context),
-        tMixCat(context),
         tMixSlider(context),
         tDryBtn(context),
         tHalfBtn(context),
         tWetBtn(context),
         vBypassBtn(context),
-        vMainCat(context),
         vRateSlider(context),
         vDepthSlider(context),
-        vMixCat(context),
         vMixSlider(context),
         vDryBtn(context),
         vHalfBtn(context),
         vWetBtn(context),
         mbcBypassBtn(context),
-        mbcMainCat(context),
         mbcRateSlider(context),
         mbcBitsSlider(context),
-        mbcMixCat(context),
         mbcMixSlider(context),
         mbcDryBtn(context),
         mbcHalfBtn(context),
         mbcWetBtn(context),
         dBypassBtn(context),
-        dMainCat(context),
         dTimeSlider(context),
         dFbSlider(context),
-        dMixCat(context),
         dMixSlider(context),
         dDryBtn(context),
         dHalfBtn(context),
         dWetBtn(context),
         rBypassBtn(context),
-        rMainCat(context),
         rSizeSlider(context),
         rDampSlider(context),
-        rMixCat(context),
         rMixSlider(context),
         rDryBtn(context),
         rHalfBtn(context),
         rWetBtn(context),
         flBypassBtn(context),
-        flMainCat(context),
         flTypeSelector(context),
         flFreqSlider(context),
         flQSlider(context),
-        flMixCat(context),
         flMixSlider(context),
         flDryBtn(context),
         flHalfBtn(context),
